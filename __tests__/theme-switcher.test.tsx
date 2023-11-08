62c90b10ba2aae340dc2b7bb9fc050911c3a4c07
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-11-08 14:00:43 +0000

    broken again somehow

--- __tests__/theme-switcher.test.tsx
+++ __tests__/theme-switcher.test.tsx
@@ -14,12 +14,12 @@ describe("ThemeSwitcher", () => {
     const themeSwitcher = screen.getByTestId("theme-switcher");
     expect(themeSwitcher).toBeInTheDocument();
   });
-  // it("renders a theme switcher with a label and icon", () => {
-  //   render(<ThemeSwitcher />);
-  //   const themeSwitcher = screen.getByTestId("theme-switcher");
-  //   const icon = screen.getByTestId("theme-switcher-icon");
-  //   expect(themeSwitcher).toContainElement(icon);
-  // });
+  it("renders a theme switcher with a label and icon", () => {
+    render(<ThemeSwitcher />);
+    const themeSwitcher = screen.getByTestId("theme-switcher");
+    const icon = screen.getByTestId("theme-switcher-icon");
+    expect(themeSwitcher).toContainElement(icon);
+  });
   // it("renders a theme switcher with a label and icon with a theme", () => {
   //   render(<ThemeSwitcher />);
   //   const themeSwitcher = screen.getByTestId("theme-switcher");
