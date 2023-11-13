80fd8f802fde234815ade1745b41d7502d4a3580
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-11-13 15:29:11 +0000

    tried to implement  a test for the actual theme switch but ternary operator is making it difficult

--- __tests__/theme-switcher.test.tsx
+++ __tests__/theme-switcher.test.tsx
@@ -26,15 +26,6 @@ describe("ThemeSwitcher", () => {
     const icon = screen.getByTestId("theme-switcher-icon");
     expect(themeSwitcher).toContainElement(icon);
   });
-  // it("renders a theme switcher with a label and icon with a theme", () => {
-  //   render(<ThemeSwitcher />);
-  //   const themeSwitcher = screen.getByTestId("theme-switcher");
-  //   const icon = screen.getByTestId("theme-switcher-icon");
-  //   const themes = screen.getByTestId("theme-switcher-themes");
-  //   const label = screen.getByText(themes[0].label);
-  //   expect(icon).toBeInTheDocument();
-  //   expect(label).toBeInTheDocument();
-  // });
   it("brings up popover when clicked", () => {
     render(<ThemeSwitcher />);
     const button = screen.getByTestId("popOverButton");
@@ -42,5 +33,17 @@ describe("ThemeSwitcher", () => {
     waitFor(() =>
       expect(screen.getByTestId("popOverPanel")).toBeInTheDocument()
     );
+  });
+  it("changes theme to selected theme on click", () => {
+    render(<ThemeSwitcher />);
+    const button = screen.getByTestId("popOverButton");
+    fireEvent.click(button);
+    waitFor(() =>
+      expect(screen.getByTestId("popOverPanel")).toBeInTheDocument()
+    );
+    const preClick = screen.getByTestId("theme-switcher-icon");
+    const key = screen.getByTestId("key");
+    fireEvent.click(key);
+    waitFor(() => expect(preClick).not.toBe(screen.getByTestId("key")));
   });
 });
